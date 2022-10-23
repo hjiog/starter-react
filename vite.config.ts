9374495934ee004fdf7efe84046c067a4b8c5237
huangjunjie.coder <huangjunjie.coder@bytedance.com>
2022-10-23 14:57:38 +0800

    chore: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Unocss from 'unocss/vite';
 import Pages from 'vite-plugin-pages';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [Unocss(), react(), Pages()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
